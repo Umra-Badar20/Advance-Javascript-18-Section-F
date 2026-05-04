d1955ba343a15e595bde2c4a7f3f044c01e3e2d5
Umra-Badar20 <umrabadar555@gmail.com>
2026-05-04 16:02:10 +0500

    Modules, clousers

---
+++ class2/app.js
@@ -0,0 +1,26 @@
+// // import { products as product , sum } from "./product.js";
+// import obj from "./product.js";
+// // console.log(product);
+// // console.log(sum(3,5));
+// console.log(obj.sum(2,3));
+
+// function greetring(){
+//     let message = "hellow"
+//     function inner(){
+//         console.log(message);
+//         let message2 = "hi"
+//     }
+//     function inner2(){
+// console.log(message2);
+
+//     }
+//     inner()
+//     inner2()
+// }
+// greetring();
+
+
+{
+    const name = "Umra"
+}
+console.log(name);
---
+++ class2/product.js
@@ -0,0 +1,20 @@
+
+ let products ={
+    name:"shampo",
+    price:900,
+}
+function sum(a,b){
+    return a+b
+}
+function subtract(a,b){
+    return a-b
+}
+export default {
+    products,
+    subtract,
+    sum
+}
+
+// export{
+//     products, sum
+// }
