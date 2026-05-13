157faab84de7a48e2b1ce24c17a519039094fb5b
Umra-Badar20 <umrabadar555@gmail.com>
2026-05-13 14:55:28 +0500

    Array methods

---
+++ class6/app.js
@@ -0,0 +1,54 @@
+
+//map method
+// let prices = [100,200,300];
+// let discountPrice = prices.map ((price)=>{
+//     return price - 10
+// })
+// console.log(discountPrice);
+
+//find
+// let num = [1,5,4,9,10,2,7];
+// let newNum = num.find(findNum);
+
+// function findNum (value) {
+//     return value > 7;
+// }
+// console.log(newNum);
+
+// reduce
+// let array = [1,2,3,5,2,1];
+// let newarray = array.reduce((val1,val2)=>{
+//     console.log(val1,val2);
+    
+//     return val1+val2
+// });
+// console.log(newarray);
+
+
+// let array = [1,2,3,5,2,1];
+// let reduce_func = (val1,val2)=>{
+//     console.log(val1,val2);
+    
+//     return val1 +val2
+// }
+
+// let newarray = array.reduce(reduce_func);
+// console.log(newarray);
+
+
+// let cart =[200,450,150,800]
+// let total = cart.reduce((sum,price)=> sum + price , 0);
+// console.log(total);
+
+//flat
+// let fruits = [
+//     ["apple","mango"]
+//     ["banana", "orange"]
+
+// ];
+// console.log(fruits.flat());
+
+
+
+
+
